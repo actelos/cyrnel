c2ca35ae4736c8ea49bd97dffbc8858ae9e452e9
0x15BA88FF <86390213+0x15BA88FF@users.noreply.github.com>
2026-04-08 06:15:30 +0000

    chore: update gitignore, and re-gen database

--- drizzle/0000_silly_mulholland_black.sql
+++ drizzle/0000_silly_mulholland_black.sql
@@ -2,7 +2,7 @@ CREATE TABLE `manifests` (
 	`id` text PRIMARY KEY NOT NULL,
 	`metadata` text NOT NULL
 );
-
+--> statement-breakpoint
 CREATE TABLE `tools` (
 	`service_id` text NOT NULL,
 	`name` text NOT NULL,
@@ -12,5 +12,5 @@ CREATE TABLE `tools` (
 	PRIMARY KEY(`service_id`, `name`),
 	FOREIGN KEY (`service_id`) REFERENCES `manifests`(`id`) ON UPDATE no action ON DELETE cascade
 );
-
+--> statement-breakpoint
 CREATE INDEX `tools_name_idx` ON `tools` (`name`);
