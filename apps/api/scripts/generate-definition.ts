1ac604872050112c51bad295c0534cfa2ecf56c7
0x15BA88FF <86390213+0x15BA88FF@users.noreply.github.com>
2026-06-09 08:57:58 +0000

    docs: update openapi definition path and symlink to docs

--- apps/api/scripts/generate-definition.ts
+++ apps/api/scripts/generate-definition.ts
@@ -3,7 +3,7 @@ import { resolve } from "node:path";
 
 import { generateOpenApiDoc } from "./openapi-definition";
 
-const outputPath = resolve(process.cwd(), "../../docs/openapi.json");
+const outputPath = resolve(process.cwd(), "./openapi.json");
 
 mkdirSync(resolve(outputPath, ".."), { recursive: true });
 writeFileSync(outputPath, JSON.stringify(generateOpenApiDoc(), null, 2));
